4db40fb595a22598e86118c5f1832da31717676e
Roel Beckers <roel@ebeck.be>
2016-10-17 23:59:27 +1100

    TalentBooster-dev initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+TalentBooster-dev
+=================
+
+A Symfony project created on October 12, 2016, 10:24 am.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roebeckers
+ * Date: 17/10/2016
+ * Time: 10:57 PM
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+
+        return $this->render('genus/show.html.twig', array(
+            'name' => $genusName,
+            'notes' => $notes
+        ));
+    }
+}
