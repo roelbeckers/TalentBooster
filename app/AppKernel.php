748506140022640895d0a2862a3765e9648ce27d
Roel Beckers <roel@ebeck.be>
2016-11-17 02:14:37 +1100

    Disable KnpMarkdownBundle in AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+            //new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
